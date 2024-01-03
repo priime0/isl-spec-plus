78ba2cd2d29bb83edda4507a67e579752ff26222
Lucas Sta Maria <lucas@priime.dev>
2024-01-03 09:36:55 -0500

    chore: update description for `isl-spec-plus`
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- isl-spec-plus/info.rkt
+++ isl-spec-plus/info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "isl-spec-plus")
 (define deps '("base" "isl-spec"))
-(define pkg-desc "isl-spec but with `write`")
+(define pkg-desc "`isl-spec`, but modified to integrate with Emacs/org-babel")
 (define version "0.1")
 (define license 'MIT)
 (define pkg-authors '(priime0))
